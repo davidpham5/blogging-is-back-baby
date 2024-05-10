a64661f1af5723e0c63d192962b38ef724ed1997
David Pham <davidpham5@gmail.com>
2024-05-10 00:55:42 -0400

    change up the title of the site

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -3,12 +3,11 @@ module.exports = {
 		ref: process.env.COMMIT_REF || null,
 		url: process.env.REPOSITORY_URL,
 	},
-	title: "Aram ZS | Digital Garden",
+	title: "David Chico Pham | Cooking Code and Eating Knowledge",
 	description: "Microblog and feed from David Pham.",
 	url: "https://davidchicopham.com",
 	feedUrl: "https://davidchicopham.com/writing/feed.xml",
 	author: {
-		name: "David Pham",
-		email: "davidpham5@hacktext.com",
+		name: "David Pham"
 	},
 };
